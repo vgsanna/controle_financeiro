1d5554753746752f31407cd7c3aebb08ea8e02f1
vinicius.gomes <vgsanna92@gmail.com>
2023-12-30 16:38:13 -0300

    Alterar rotas e criar controllers e views para as mesmas.

---
+++ app/Http/Controllers/LayoutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+use App\Models\Usuarios;
+
+class LayoutController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        $usuarios = Usuarios::find(1);
+        echo $usuarios;
+        return view('index');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LayoutController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/index', function () {
+//     return view('index');
+// });
+
+Route::get('/index', [LayoutController::class, 'index']);
